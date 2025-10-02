d982099219c48e5d08150fe6d01d2b3559ee69f6
Jaskaran <write2jaskaransingh@gmail.com>
2025-10-02 22:50:42 +0530

    fix taskbar and title bar issue

--- ReStore/App.xaml.cs
+++ ReStore/App.xaml.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Diagnostics;
+using System.Threading;
+using System.IO.Pipes;
+using System.IO;
 using System.Windows;
 using ReStore.Views;
 using ReStore.Views.Pages;
@@ -12,9 +15,31 @@ namespace ReStore
     public partial class App : Application
     {
         private SystemTrayManager? _trayManager;
+        private static Mutex? _instanceMutex;
+        private const string MUTEX_NAME = "ReStore_SingleInstance_Mutex";
+        private const string PIPE_NAME = "ReStore_CommandPipe";
+        private Thread? _pipeServerThread;
+        private bool _isRunning = true;
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            bool createdNew;
+            _instanceMutex = new Mutex(true, MUTEX_NAME, out createdNew);
+
+            if (!createdNew)
+            {
+                if (e.Args.Length > 0)
+                {
+                    SendCommandToExistingInstance(e.Args[0]);
+                }
+                else
+                {
+                    BringExistingInstanceToFront();
+                }
+                Shutdown();
+                return;
+            }
+
             base.OnStartup(e);
             AppDomain.CurrentDomain.UnhandledException += (_, args) =>
             {
@@ -45,15 +70,134 @@ namespace ReStore
                 WindowEffects.ApplySystemBackdrop(mainWindow);
                 WindowEffects.FixMaximizedBounds(mainWindow);
             };
+            
+            if (e.Args.Length > 0)
+            {
+                HandleCommandLineArgs(e.Args, mainWindow);
+            }
+            
             mainWindow.Show();
+            
+            _pipeServerThread = new Thread(ListenForCommands);
+            _pipeServerThread.IsBackground = true;
+            _pipeServerThread.Start();
+        }
+
+        private void ListenForCommands()
+        {
+            while (_isRunning)
+            {
+                try
+                {
+                    using (var pipeServer = new NamedPipeServerStream(PIPE_NAME, PipeDirection.In, 1))
+                    {
+                        pipeServer.WaitForConnection();
+                        
+                        using (var reader = new StreamReader(pipeServer))
+                        {
+                            var command = reader.ReadToEnd();
+                            
+                            Dispatcher.Invoke(() =>
+                            {
+                                if (MainWindow is MainWindow mw)
+                                {
+                                    HandleCommand(command, mw);
+                                }
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine($"Pipe server error: {ex}");
+                }
+            }
+        }
+
+        private void SendCommandToExistingInstance(string command)
+        {
+            try
+            {
+                using (var pipeClient = new NamedPipeClientStream(".", PIPE_NAME, PipeDirection.Out))
+                {
+                    pipeClient.Connect(1000);
+                    
+                    using (var writer = new StreamWriter(pipeClient))
+                    {
+                        writer.Write(command);
+                        writer.Flush();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine($"Failed to send command to existing instance: {ex}");
+            }
+        }
+
+        private void BringExistingInstanceToFront()
+        {
+            SendCommandToExistingInstance("/show");
+        }
+
+        private void HandleCommand(string command, MainWindow mainWindow)
+        {
+            mainWindow.Show();
+            mainWindow.WindowState = WindowState.Normal;
+            mainWindow.Activate();
+            
+            if (command == "/startWatcher")
+            {
+                ExecuteWatcherAction(mainWindow, true);
+            }
+            else if (command == "/stopWatcher")
+            {
+                ExecuteWatcherAction(mainWindow, false);
+            }
+        }
+
+        private void ExecuteWatcherAction(MainWindow mainWindow, bool start)
+        {
+            mainWindow.Dispatcher.InvokeAsync(async () =>
+            {
+                await System.Threading.Tasks.Task.Delay(100);
+                var frame = mainWindow.FindName("ContentFrame") as System.Windows.Controls.Frame;
+                
+                if (frame?.Content is not DashboardPage)
+                {
+                    frame?.Navigate(new DashboardPage());
+                    await System.Threading.Tasks.Task.Delay(100);
+                }
+                
+                if (frame?.Content is DashboardPage dashboard)
+                {
+                    var btnName = start ? "StartWatcherBtn" : "StopWatcherBtn";
+                    var btn = dashboard.FindName(btnName) as System.Windows.Controls.Button;
+                    btn?.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
+                }
+            });
+        }
+
+        private void HandleCommandLineArgs(string[] args, MainWindow mainWindow)
+        {
+            foreach (var arg in args)
+            {
+                HandleCommand(arg, mainWindow);
+            }
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
+            _isRunning = false;
+            
             if (MainWindow is MainWindow mw)
             {
                 mw.TrayManager?.Dispose();
             }
+            
+            _instanceMutex?.ReleaseMutex();
+            _instanceMutex?.Dispose();
+            
             base.OnExit(e);
         }
     }
--- ReStore/Views/MainWindow.xaml.cs
+++ ReStore/Views/MainWindow.xaml.cs
@@ -7,11 +7,56 @@ using Wpf.Ui.Appearance;
 using ReStore.Services;
 using ReStore.Interop;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
 
 namespace ReStore.Views
 {
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+        
+        [DllImport("user32.dll")]
+        private static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+        
+        private const uint MONITOR_DEFAULTTONEAREST = 2;
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MONITORINFO
+        {
+            public int cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public uint dwFlags;
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        }
+        
         private SolidColorBrush _navHoverBrush = new SolidColorBrush(Color.FromArgb(0x33, 0xFF, 0xFF, 0xFF));
         private SolidColorBrush _navSelectedBrush = new SolidColorBrush(Color.FromArgb(0x4D, 0xFF, 0xFF, 0xFF));
         private SolidColorBrush _captionHoverBrush = new SolidColorBrush(Color.FromArgb(0x1A, 0xFF, 0xFF, 0xFF));
@@ -108,6 +153,108 @@ namespace ReStore.Views
             if (maxBtn != null) maxBtn.Click += (_, __) => ToggleMaximize();
             if (closeBtn != null) closeBtn.Click += OnCloseButtonClick;
 
+            SourceInitialized += OnSourceInitialized;
+            Loaded += OnWindowLoaded;
+        }
+        
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_GETMINMAXINFO = 0x0024;
+            
+            if (msg == WM_GETMINMAXINFO)
+            {
+                var mmi = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                var monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+                
+                if (monitor != IntPtr.Zero)
+                {
+                    var monitorInfo = new MONITORINFO();
+                    monitorInfo.cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+                    GetMonitorInfo(monitor, ref monitorInfo);
+                    
+                    var workArea = monitorInfo.rcWork;
+                    var monitorArea = monitorInfo.rcMonitor;
+                    
+                    mmi.ptMaxPosition.X = workArea.Left - monitorArea.Left;
+                    mmi.ptMaxPosition.Y = workArea.Top - monitorArea.Top;
+                    mmi.ptMaxSize.X = workArea.Right - workArea.Left;
+                    mmi.ptMaxSize.Y = workArea.Bottom - workArea.Top;
+                    
+                    Marshal.StructureToPtr(mmi, lParam, true);
+                    handled = true;
+                }
+            }
+            
+            return IntPtr.Zero;
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            SetupTaskbarJumpList();
+            
+            var onStarted = () => Dispatcher.Invoke(SetupTaskbarJumpList);
+            var onStopped = () => Dispatcher.Invoke(SetupTaskbarJumpList);
+            WatcherService.Instance.SetCallbacks(onStarted, onStopped, null);
+        }
+
+        private void SetupTaskbarJumpList()
+        {
+            var jumpList = new JumpList();
+            jumpList.ShowFrequentCategory = false;
+            jumpList.ShowRecentCategory = false;
+
+            var exePath = Environment.ProcessPath ?? System.Diagnostics.Process.GetCurrentProcess().MainModule?.FileName;
+            if (string.IsNullOrEmpty(exePath))
+                return;
+
+            var isRunning = WatcherService.Instance.IsRunning;
+
+            if (isRunning)
+            {
+                var stopTask = new JumpTask
+                {
+                    Title = "Stop Watcher",
+                    Description = "Stop the file watcher service",
+                    ApplicationPath = exePath,
+                    Arguments = "/stopWatcher",
+                    IconResourcePath = exePath
+                };
+                jumpList.JumpItems.Add(stopTask);
+            }
+            else
+            {
+                var startTask = new JumpTask
+                {
+                    Title = "Start Watcher",
+                    Description = "Start the file watcher service",
+                    ApplicationPath = exePath,
+                    Arguments = "/startWatcher",
+                    IconResourcePath = exePath
+                };
+                jumpList.JumpItems.Add(startTask);
+            }
+
+            JumpList.SetJumpList(Application.Current, jumpList);
+        }
+
+        public void UpdateJumpList()
+        {
+            SetupTaskbarJumpList();
+        }
+
+        private void OnSourceInitialized(object? sender, EventArgs e)
+        {
+            var handle = new WindowInteropHelper(this).Handle;
+            var source = HwndSource.FromHwnd(handle);
+            if (source != null)
+            {
+                source.AddHook(WindowProc);
+            }
+            UpdateWindowState();
+        }
+
+        private void Window_StateChanged(object? sender, EventArgs e)
+        {
             UpdateWindowState();
         }
 
@@ -219,20 +366,7 @@ namespace ReStore.Views
                 chrome.CornerRadius = isMaximized ? new CornerRadius(0) : new CornerRadius(8);
             }
 
-            if (isMaximized)
-            {
-                var resizeBorderThickness = chrome?.ResizeBorderThickness.Left ?? 6;
-                MainGrid.Margin = new Thickness(
-                    resizeBorderThickness,
-                    resizeBorderThickness, 
-                    resizeBorderThickness, 
-                    resizeBorderThickness
-                );
-            }
-            else
-            {
-                MainGrid.Margin = new Thickness(0);
-            }
+            MainGrid.Margin = new Thickness(0);
         }
 
         private void SetSelectedNav(Button selected)
